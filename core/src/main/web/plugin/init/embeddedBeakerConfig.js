eef49f2acdf185a4814114b98f705ab1f45fda00
Greg Kinman <gregakinman@gmail.com>
2016-01-16 01:34:56 -0600

    Removed unnecessary button code.

--- core/src/main/web/plugin/init/embeddedBeakerConfig.js
+++ core/src/main/web/plugin/init/embeddedBeakerConfig.js
@@ -222,22 +222,6 @@
                                      bkHelper.getFileSystemFileChooserStrategy()
                                  );
                              }
-                           },
-                           {  
-                             name: "Open from Google Drive (.bkr)",
-                             id: "open-menuitem",
-                             reducedName: "Open from Drive...",
-                             tooltip: "Open a bkr notebook file from Google Drive",
-                             sortorder: 110,
-                             action: function() {
-                                // TODO this is temporary
-                                 bkHelper.showModalDialog(
-                                     function(originalUrl) {
-                                       bkHelper.openNotebook(originalUrl);
-                                     },
-                                     JST['template/opennotebook']({homedir: homeDir, extension: '.bkr'}),
-                                     bkHelper.getFileSystemFileChooserStrategy()
-                                 );
                            }
                            ]
                  }
